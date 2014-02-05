9476118f397d3831533e39009bd309930f8cd09e
deemoshea <deemoshea@192.168.1.4>
2014-02-05 18:21:20 +0000

    Initial Commit

---
+++ JohnTestGit/src/Test/main.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is a test");
+		System.out.println("This is test 1");
+
+	}
+
+}
