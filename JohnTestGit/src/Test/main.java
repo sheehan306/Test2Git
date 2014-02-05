461539707a6c58027b3cbf558ea72e702033c9f7
deemoshea <deemoshea@192.168.1.4>
2014-02-05 21:37:11 +0000

    Second Change

--- JohnTestGit/src/Test/main.java
+++ JohnTestGit/src/Test/main.java
@@ -8,7 +8,12 @@ public class main {
 		System.out.println("This is a test");
 		System.out.println("This is test 1");
 		System.out.println("This is test 2");
+		System.out.println("This is test 3");
+		System.out.println("This is test 4");
+		System.out.println("Hello");
 
+		
+		
 	}
 
 }
